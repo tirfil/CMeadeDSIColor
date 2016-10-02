78c50351bc4d24cfcf2bd009385cf5b0078dc387
Philippe THIRION <philippe.thirion@laposte.net>
2016-10-02 13:21:37 +0200

    missing file + focus script

---
+++ focus.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import time
+import signal
+
+path=r"~/Documents/DSI/CMeadeDSIColor/"
+
+if not os.path.isdir("work"):
+	os.mkdir("work")
+os.chdir("work")
+
+for i in range(10):
+	cmd = path+"MeadeDSIColor -e 100 -g 10 -o 0 -f"
+	os.system(cmd)
+	cmd = path+"cmyg2tiff cmyg.fits"
+	os.system(cmd)
+	pid = os.fork()
+	if not pid:
+		cmd = "eog color.tif"
+		ret = os.system(cmd)
+		exit(0)
+	else:
+		time.sleep(5)
+		print "kill",pid
+		os.kill(pid+2,signal.SIGTERM)
+		os.wait()
+	os.remove("cmyg.fits")
+	os.remove("color.tif")
+	
+	
