16a8d526c721f62169ee3484195acd02856653c9
Philippe THIRION <philippe.thirion@laposte.net>
2017-04-02 09:34:40 +0200

    change color algo - color order

--- compile.sh
+++ compile.sh
@@ -4,4 +4,5 @@ gcc cmyg2rgb3.c -lcfitsio -o cmyg2rgb3
 gcc cmyg2tiff.c -lcfitsio -ltiff -o cmyg2tiff
 gcc cmyg2png.c -lpng -lcfitsio -o cmyg2png
 gcc cmyg2png3.c -lpng -lcfitsio -o cmyg2png3
+gcc cmyg2fits.c -lcfitsio -o cmyg2fits
 
